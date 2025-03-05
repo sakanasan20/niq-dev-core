d07c786b265618ef913beb34e72b1aaaba841c0b
Nick Chen <nick.chen@cmp.com.tw>
2025-03-05 12:07:09 +0800

    Initial commit

---
+++ src/main/java/com/niqdev/NiqDevCoreApplication.java
@@ -0,0 +1,13 @@
+package com.niqdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiqDevCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiqDevCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niqdev/NiqDevCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niqdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiqDevCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
