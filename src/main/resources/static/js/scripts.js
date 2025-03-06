a02602c59cc61f5c53b56d20a309c7c60de57902
Nick Chen <nick.chen@cmp.com.tw>
2025-03-06 16:08:56 +0800

    feat: 增加子菜單收合邏輯

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
 		// 檢查 Sidebar 是否收縮，來隱藏/顯示子菜單圖示
 		if ($("#sidebar").hasClass("collapsed")) {
 			$("#submenuToggleIcon").hide(); // Sidebar 縮小時，隱藏折疊圖示
+			$("#submenu").collapse('hide');  // 假設所有的子菜單都有 ".submenu" 類別
 		} else {
 			$("#submenuToggleIcon").show(); // Sidebar 展開時，顯示折疊圖示
 		}
@@ -13,10 +14,10 @@ $(document).ready(function() {
 
 	// 點擊 "設定" 選單，展開或收起子菜單
 	$("#collapseSetting").click(function() {
-		$("#submenu").collapse('toggle');
-
 		// 切換 Chevron 圖示 (只有在 Sidebar 展開時才變動)
 		if (!$("#sidebar").hasClass("collapsed")) {
+			$("#submenu").collapse('toggle');
+			
 			if ($("#submenuToggleIcon").hasClass("bi-chevron-down")) {
 				$("#submenuToggleIcon").removeClass("bi-chevron-down").addClass("bi-chevron-up");
 			} else {
